30a85274afa1acf846bce20ae9b7eb9651e2158e
Anders Holden Deleuran <andersdeleuran@outlook.com>
2014-07-17 13:50:01 +0200

    Pushed files

---
+++ src/MeshCurvature.py
@@ -0,0 +1,139 @@
+﻿"""
+Analyse mesh curvature. Curvature is defined here as the angle between a vertex 
+normal and the vector from this vertex to its neighbours. Zero curvature is thus 
+equal to 90 degrees. However to evaluate positive and negative curvatures 90 is 
+subtracted from this value. Meaning that negative curvature will be a negative 
+value and positive curvature a positive value.
+-
+Name: MeshCurvature
+Updated: 140614
+Author: Anders Holden Deleuran (CITA/KADK)
+Copyright: Creative Commons - Attribution 4.0 International
+Contact: adel@kadk.dk
+
+    Args:
+        Toggle: Activates the component.
+        Mesh: The mesh to analyse.
+        Mode: The curvature mode to calculate ("min","max" or "mean").
+        NegativeOff: If True negative curvature is treated as if positive.
+    Returns:
+        Mesh: A mesh colored by its curvature values.
+        Curvature: The curvature values for each vertex.
+        Colors: The curvature color for each vertex.
+        CurvatureSum: The total curvature.
+        CurvatureBounds: The minimum and maximum curvature.
+        CurvatureSorted: Sorted curvature values (for graph mapping).
+        MeshArea: The area of the mesh.
+"""
+
+import Rhino as rc
+import math
+
+
+# Set component name/nick
+ghenv.Component.Name = "MeshCurvature"
+ghenv.Component.NickName = "MeshCurvature"
+
+def meshCurvature(mesh,mode,negativeOff):
+    
+    """ Calculate the curvature of a mesh using vertex normal angle 
+    from each vertex to its neighbours """
+    
+    # Output list
+    curvature = []
+    
+    # Iterate mesh vertices
+    for i in range(mesh.Vertices.Count):
+        
+        # Get vertex, vertex normal and vertex neighbour IDs
+        centerVertex = mesh.Vertices.Item[i]
+        centerNormal = mesh.Normals.Item[i]
+        neighbourIDs = mesh.Vertices.GetConnectedVertices(i)
+        
+        # Iterate neighbouring vertices
+        angles = []
+        for j in neighbourIDs:
+            
+            # Make vector from vertex to neighbour and check its length
+            neighbourVertex = mesh.Vertices.Item[j]
+            neighbourDirection = neighbourVertex - centerVertex
+            if neighbourDirection.Length > 0:
+                
+                # Calculate angle between normal and new vector
+                a = rc.Geometry.Vector3d.VectorAngle(centerNormal,neighbourDirection)
+                
+                # Subtract it from 90 and optionally get absolute value (no negative curvature)
+                a = math.degrees(a) - 90
+                if negativeOff:
+                    a = abs(a)
+                    
+                # Add to list
+                angles.append(a)
+                
+        # Calculate curvature depending on mode
+        if mode == "min":
+            curvature.append(min(angles))
+            
+        elif mode == "max":
+            curvature.append(max(angles))
+            
+        elif mode == "mean":
+            curvature.append(sum(angles)/len(angles))
+            
+    return curvature
+
+def remapValues(values,targetMin,targetMax):
+    
+    """ Remap numbers into a new numeric domain """
+    
+    # Check that different values exist
+    if len(set(values)) > 1:
+        
+        remappedValues = []
+        
+        # Get sourceDomain min and max
+        srcMin = min(values)
+        srcMax = max(values)
+        
+        # Iterate the values and remap them
+        for v in values:
+            rv = ((v-srcMin)/(srcMax-srcMin))*(targetMax-targetMin)+targetMin
+            remappedValues.append(rv)
+            
+        return remappedValues
+        
+    # Else return targetMax for each value
+    else:
+        return [targetMax]*len(values)
+
+def mapValueListAsColors(values):
+    
+    """ Make a list of HSL color where the values are mapped onto a
+    0.0 - 0.7 hue domain. Meaning that low values will be red, medium
+    values green and large values blue """
+    
+    colors = []
+    remappedValues = remapValues(values,0.0,0.7)
+    for v in remappedValues:
+        c = rc.Display.ColorHSL(v,1.0,0.5).ToArgbColor()
+        colors.append(c)
+        
+    return colors
+
+def colorMesh(mesh,colors):
+    
+    """ Color mesh vertices by list of colors """
+    
+    for i,c in enumerate(colors):
+        mesh.VertexColors.SetColor(i,c)
+
+# Function calls and GH output
+if Toggle:
+    if Mesh:
+        Curvature = meshCurvature(Mesh,Mode,NegativeOff)
+        CurvatureSorted = sorted(Curvature)
+        CurvatureSum = round(sum(Curvature),2)
+        CurvatureBounds = (round(min(Curvature),2)),(round(max(Curvature),2))
+        Colors = mapValueListAsColors(Curvature)
+        CMesh = Mesh
+        colorMesh(CMesh,Colors)
---
+++ src/MeshDrainage.py
@@ -0,0 +1,89 @@
+﻿"""
+Simulates drainage in order to analyse a mesh for local pooling of rain water and snow.
+-
+Name: MeshDrainageAnalysis
+Updated: 140621
+Author: Anders Holden Deleuran (CITA/KADK)
+Copyright: Creative Commons - Attribution 4.0 International
+Contact: adel@kadk.dk
+
+    Args:
+        Toggle: Activates the component.
+        Mesh: The mesh to analyse.
+        ParticleCount: The amount of particles to simulate (randomly picked from the mesh vertices).
+        MaxSteps: The amount steps to iteratively move the particles.
+        Tolerance: The precision with which the particles are moved.
+        RandomSeed: The random seed with which the particles are picked.
+        
+    Returns:
+        DrainCurves: Curves constructed through the drain positions of each particle.
+        EndPoints: The final position of the particles.
+"""
+
+import Rhino as rc
+import random
+import System.Threading.Tasks as tasks
+
+# Set component name/nick
+ghenv.Component.Name = "MeshDrainage"
+ghenv.Component.NickName = "MeshDrainage"
+
+def makeDrainMeshPaths(mesh,startPoints,maxSteps,tolerance):
+    
+    """ Estimates the trail of a drainage path on a mesh. Based on Benjamin 
+    Golders concept and Remy's VB code found here:
+    http://www.grasshopper3d.com/forum/topics/drainage-direction-script """
+    
+    # Return list
+    drainPaths = []
+    
+    # Task function
+    def drainPath(pt):
+        
+        # Particle list and current particle plane
+        particles = []
+        paPl = rc.Geometry.Plane(pt,rc.Geometry.Vector3d.ZAxis)
+        
+        for j in range(maxSteps):
+            
+            # Get the point on the mesh closest to the particle
+            meshPt = mesh.ClosestMeshPoint(paPl.Origin,0.0)
+            paPl = rc.Geometry.Plane(meshPt.Point,mesh.NormalAt(meshPt))
+            
+            # Check that first step has been taken and that current step is down slope
+            if j and paPl.Origin.Z > particles[-1].Z:
+                break
+            else:
+                # Add particle to list
+                particles.append(paPl.Origin)
+                
+                # Move particle down slope
+                an = rc.Geometry.Vector3d.VectorAngle(paPl.XAxis,-rc.Geometry.Vector3d.ZAxis,paPl)
+                paPl.Rotate(an,paPl.ZAxis)
+                paPl.Translate(paPl.XAxis*tolerance)
+                
+        # Make drain curve
+        if len(particles) > 1:
+            
+            crv = rc.Geometry.Curve.CreateControlPointCurve(particles)
+            drainPaths.append(crv)
+            
+    # Call task function
+    tasks.Parallel.ForEach(startPoints,drainPath)
+    
+    return drainPaths
+
+if Toggle and Mesh:
+    
+    # Get mesh vertices
+    Vertices = Mesh.Vertices.ToPoint3dArray()
+    
+    # Pick random vertices
+    if ParticleCount > len(Vertices):
+        ParticleCount = len(Vertices)
+    random.seed(RandomSeed)
+    Vertices = random.sample(Vertices,ParticleCount)
+    
+    # Make drain curves
+    DrainCurves = makeDrainMeshPaths(Mesh,Vertices,MaxSteps,Tolerance)
+    EndPoints = [crv.PointAtEnd for crv in DrainCurves]
