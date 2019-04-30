4253465554ad29fab1bc263e4b7d65bfc74c7d32
Anders Holden Deleuran <andersdeleuran@outlook.com>
2019-04-30 11:32:22 +0200

    Update MeshDrainage.py

--- src/MeshDrainage.py
+++ src/MeshDrainage.py
@@ -1,39 +1,33 @@
-﻿"""
-Simulates drainage in order to analyse a mesh for local pooling of rain water and snow.
--
-Name: MeshDrainageAnalysis
-Updated: 140621
-Author: Anders Holden Deleuran (CITA/KADK)
-Copyright: Creative Commons - Attribution 4.0 International
-GitHub: www.github.com/AndersDeleuran/MeshAnalysis
-Contact: adel@kadk.dk
-
-    Args:
-        Toggle: Activates the component.
-        Mesh: The mesh to analyse.
-        ParticleCount: The amount of particles to simulate (randomly picked from the mesh vertices).
-        MaxSteps: The amount steps to iteratively move the particles.
-        Tolerance: The precision with which the particles are moved.
-        RandomSeed: The random seed with which the particles are picked.
-        
-    Returns:
-        DrainCurves: Curves constructed through the drain positions of each particle.
-        EndPoints: The final position of the particles.
+"""
+Geometric drainage simualtion model for analysing a mesh for pooling of rain water.
+    Inputs:
+        Mesh: The mesh to analyse {item,mesh}
+        ParticleCount: Amount of particles randomly picked from Mesh vertices {item,int}
+        MaxSteps: Amount of steps to iteratively move the particles {item,int}
+        StepSize: The distance by which the particles are moved at each step {item,float}
+        RandomSeed: The random seed with which the particles are picked {item,int}
+        Threaded: Toggle multithreading {item,bool}
+    Outputs:
+        DrainagePaths: Polylines through the paths of each particle {list,polyline}
+    Remarks:
+        Author: Anders Holden Deleuran (BIG Compute/Ideas/Tech)
+        License:
+        Version: 190311
 """
 
 import Rhino as rc
 import random
 import System.Threading.Tasks as tasks
+import Grasshopper.Kernel.Types as gkt
 
 # Set component name/nick
-ghenv.Component.Name = "MeshDrainage"
-ghenv.Component.NickName = "MeshDrainage"
+ghenv.Component.Name = "MeshDrainagePaths"
+ghenv.Component.NickName = "MDP"
 
-def makeDrainMeshPaths(mesh,startPoints,maxSteps,tolerance):
+def makeDrainMeshPaths(mesh,startPoints,maxSteps,stepSize,threaded):
     
-    """ Estimates the trail of a drainage path on a mesh. Based on Benjamin 
-    Golders concept and Remy's VB code found here:
-    http://www.grasshopper3d.com/forum/topics/drainage-direction-script """
+    """ Estimates drainage paths on a mesh. Based on Benjamin Golders concept
+    found here: www.grasshopper3d.com/forum/topics/drainage-direction-script """
     
     # Return list
     drainPaths = []
@@ -41,50 +35,50 @@ def makeDrainMeshPaths(mesh,startPoints,maxSteps,tolerance):
     # Task function
     def drainPath(pt):
         
-        # Particle list and current particle plane
+        # Make particle list and set current particle plane
         particles = []
         paPl = rc.Geometry.Plane(pt,rc.Geometry.Vector3d.ZAxis)
-        
-        for j in range(maxSteps):
+        for i in range(maxSteps):
             
-            # Get the point on the mesh closest to the particle
-            meshPt = mesh.ClosestMeshPoint(paPl.Origin,0.0)
-            paPl = rc.Geometry.Plane(meshPt.Point,mesh.NormalAt(meshPt))
-            
-            # Check that first step has been taken and that current step is down slope
-            if j and paPl.Origin.Z > particles[-1].Z:
-                break
-            else:
-                # Add particle to list
-                particles.append(paPl.Origin)
+            # Get point on mesh closest to current particle position
+            meshPt = mesh.ClosestMeshPoint(paPl.Origin,0.00)
+            if meshPt:
+                paPl = rc.Geometry.Plane(meshPt.Point,mesh.NormalAt(meshPt))
                 
-                # Move particle down slope
-                an = rc.Geometry.Vector3d.VectorAngle(paPl.XAxis,-rc.Geometry.Vector3d.ZAxis,paPl)
-                paPl.Rotate(an,paPl.ZAxis)
-                paPl.Translate(paPl.XAxis*tolerance)
-                
-        # Make drain curve
+                # Check first step has been taken and that current step is down slope
+                if i and paPl.Origin.Z > particles[-1].Z:
+                    break
+                else:
+                    # Record particle position and move down slope
+                    particles.append(paPl.Origin)
+                    an = rc.Geometry.Vector3d.VectorAngle(paPl.XAxis,-rc.Geometry.Vector3d.ZAxis,paPl)
+                    paPl.Rotate(an,paPl.ZAxis)
+                    paPl.Translate(paPl.XAxis*stepSize)
+                    
+        # Make drain polyline
         if len(particles) > 1:
-            
-            crv = rc.Geometry.Curve.CreateControlPointCurve(particles)
-            drainPaths.append(crv)
-            
-    # Call task function
-    tasks.Parallel.ForEach(startPoints,drainPath)
-    
+            pl = rc.Geometry.Polyline(particles)
+            drainPaths.append(pl)
+                
+    # Call task function (use non-threaded for debugging)
+    if threaded:
+        tasks.Parallel.ForEach(startPoints,drainPath)
+    else:
+        [drainPath(pt) for pt in startPoints]
+        
     return drainPaths
 
-if Toggle and Mesh:
+if Mesh:
     
-    # Get mesh vertices
-    Vertices = Mesh.Vertices.ToPoint3dArray()
-    
-    # Pick random vertices
-    if ParticleCount > len(Vertices):
-        ParticleCount = len(Vertices)
+    # Get mesh vertices and pick random
+    vts = Mesh.Vertices.ToPoint3dArray()
+    if ParticleCount > len(vts):
+        ParticleCount = len(vts)
     random.seed(RandomSeed)
-    Vertices = random.sample(Vertices,ParticleCount)
+    vts = random.sample(vts,int(ParticleCount))
     
-    # Make drain curves
-    DrainCurves = makeDrainMeshPaths(Mesh,Vertices,MaxSteps,Tolerance)
-    EndPoints = [crv.PointAtEnd for crv in DrainCurves]
+    # Make paths and output to GH
+    drainagePaths = makeDrainMeshPaths(Mesh,vts,int(MaxSteps),StepSize,Threaded)
+    DrainagePaths = [gkt.GH_Curve(pl.ToNurbsCurve()) for pl in drainagePaths]
+else:
+    DrainagePaths = []
